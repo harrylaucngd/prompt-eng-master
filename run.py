54b70043ee12ec2a2f72f2f081b59e6694540b35
harrylaucngd <harrylaucngd@gmail.com>
2023-11-02 22:19:12 +0800

    Tests on naive enzyme dataset.

--- run.py
+++ run.py
@@ -3,13 +3,13 @@ from inference import infer
 
 setting_parameters = {
     # Input evaluation sets. Options: "enzyme", "small_molecule", "crystal_material" and "All".
-    "input": "small_molecule",
+    "input": "enzyme",
     # Output direction. Append answer json file to results folder named after testing date.
-    "output": "results/1024/",
+    "output": "results/1102/",
     # LLM models. Options: "GPT-3.5", "GPT-4", "LLaMA2-7B", "LLaMA2-13B", "LLaMA2-70B".
     "model_config": "GPT-3.5",
     # Prompt configurations. Options: "zero-shot", "expert", "few-shot", "zero-shot-CoT", "few-shot-CoT", "few-shot-CoT-critique", "decomposed".
-    "agent_config": "few-shot-CoT-critique",
+    "agent_config": "zero-shot-CoT",
     # Number of few-shot examples (use multi-shot if set to >1). Default: 2.
     "n_examples": 2,
     # Number of llm-generated answers. Default: 2.
